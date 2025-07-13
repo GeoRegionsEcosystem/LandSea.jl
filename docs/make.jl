71e88d2456aaa3367f3bdb9c62c1346abf2e32e7
Nathanael Wong <natgeo.wong@outlook.com>
2025-07-12 23:37:14 -0400

    Updates to favicon and DocumenterVitepress

--- docs/make.jl
+++ docs/make.jl
@@ -35,7 +35,7 @@ for file in files
     rm(file)
 end
 
-deploydocs(;
+DocumenterVitepress.deploydocs(;
     repo      = "github.com/GeoRegionsEcosystem/LandSea.jl.git",
     target    = "build", # this is where Vitepress stores its output
     devbranch = "main",
